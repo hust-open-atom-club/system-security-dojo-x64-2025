2b97944e1e97ba1bf09dd07a205c2ab701fbd52a
Dongliang Mu <dzm91@hust.edu.cn>
2025-08-28 18:33:07 +0800

    add teaching level for integer overflow

--- integer-overflow/teaching-level/teaching-level.c
+++ integer-overflow/teaching-level/teaching-level.c
@@ -1,24 +1,22 @@
-#include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 void print_desc()
 {
 	printf("###\n");
-	printf("### Welcome to ./integer-overflow-level3.0!\n");
+	printf("### Welcome to ./teaching-level!\n");
 	printf("###\n");
 	printf("\n");
 	printf("This challenge will use integer overflow to bypass some checks, and print the flag\n");
 }
 
-long read_hex_long()
+unsigned long read_hex_long()
 {
 	char buf[0x10];
 	char *ptr;
 	read(0, buf, 0x10);
-	return strtol(buf, &ptr, 16);
+	return strtoul(buf, &ptr, 16);
 }
 
 void print_flag()
@@ -46,34 +44,20 @@ void print_flag()
 
 int main()
 {
-	int64_t i;
-	int32_t j;
-	int16_t k;
-	bool result = true;
+	unsigned long i, j;
 
 	print_desc();
 
         puts("Give me your input");
 	i = read_hex_long();
+	j = read_hex_long();
 
-	if ((i > 0) || ((uint64_t)i) < 0xFFFFFFFE00000000) {
-	    result = false;
+	if (i + j != 0x80) {
+		printf("You need to provide two numbers, with the sum as 0x80\n");
+		return -1;
 	}
 
-	j = i;
-
-
-	if ((j > 0) || ((uint32_t)j) > 0x88880000) {
-	    result = false;
-	}
-
-	k = i;
-
-	if ((k > 0) || ((uint16_t)k) != 0x9527) {
-		result = false;
-	}
-
-	if (result)
+	if ((i > 0x80) && (j > 0x80))
 		print_flag();
 	else
 	 	printf("Please leverage the underlying integer overflow to bypass the condition\n");
